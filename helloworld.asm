ebc9e6dd4d9c281f6eac4966931ed86f8d0e0761
coderjojo <45591748+coderjojo@users.noreply.github.com>
2019-12-10 00:39:22 +0530

    Added Assignement 1 & 2

---
+++ helloworld.asm
@@ -0,0 +1,15 @@
+section .data
+	m1 db "Assembly programming is Hard AF"
+	l1 equ $-m1
+
+section .text
+global _start
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, m1
+	mov rdx, l1
+syscall
+mov rax, 60
+mov rdi, 0
+syscall
